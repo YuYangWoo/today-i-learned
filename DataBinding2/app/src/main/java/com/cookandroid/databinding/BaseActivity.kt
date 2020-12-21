135f45abbd2252cbb3f8ddf23c7af11df858dbe1
yuyw0712 <yuyw0712@naver.com>
2020-12-21 23:07:31 +0900

    BaseActivity 구현

---
+++ DataBinding2/app/src/main/java/com/cookandroid/databinding/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.cookandroid.databinding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T: ViewDataBinding>(private val layoutId: Int) : AppCompatActivity() {
+    lateinit var binding:T
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+    }
+}
--- DataBinding2/app/src/main/java/com/cookandroid/databinding/MainActivity.kt
+++ DataBinding2/app/src/main/java/com/cookandroid/databinding/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cookandroid.databinding
 
+import android.app.Activity
 import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,21 +8,15 @@ import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.cookandroid.databinding.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         var person1 = Person("양우",24)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.person = person1
         binding.txt3.text = "5"
-
-
-
     }
 
     data class Person(var name: String, var age:Int)
