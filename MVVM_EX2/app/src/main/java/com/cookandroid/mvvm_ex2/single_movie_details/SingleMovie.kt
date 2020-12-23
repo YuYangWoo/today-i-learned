b3fc2eb6c1f214c39030ff60065990d1bf66b8db
yuyw0712 <yuyw0712@naver.com>
2020-12-23 22:41:29 +0900

    MVVM패턴 예제 1

---
+++ MVVM_EX2/app/src/main/java/com/cookandroid/mvvm_ex2/single_movie_details/SingleMovie.kt
@@ -0,0 +1,12 @@
+package com.cookandroid.mvvm_ex2.single_movie_details
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cookandroid.mvvm_ex2.R
+
+class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
