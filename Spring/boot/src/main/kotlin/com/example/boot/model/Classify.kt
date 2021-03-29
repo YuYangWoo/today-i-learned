9ecb9feeb43ab124c63a827ceb77c4c4d9b29da4
YuYangWoo <59405161+YuYangWoo@users.noreply.github.com>
2021-03-29 17:58:02 +0900

    여러가지 형태의 서버데이터 처리

---
+++ Spring/boot/src/main/kotlin/com/example/boot/model/Classify.kt
@@ -0,0 +1,23 @@
+package com.example.boot.model
+
+import com.kaonsoft.mission.InformationRequest
+import com.kaonsoft.mission.InformationResponse
+
+class Classify() {
+
+    fun admin(info: InformationRequest): InformationResponse  {
+        return if(info.name == "유양우") {
+            InformationResponse(result = true, admin = true)
+        } else {
+            InformationResponse(result = true, admin = false)
+        }
+    }
+
+    fun urlAdmin(info: HashMap<String, Any>): InformationResponse {
+        return if(info["name"] == "유양우") {
+            InformationResponse(result = true, admin = true)
+        } else {
+            InformationResponse(result = true, admin = false)
+        }
+    }
+}
